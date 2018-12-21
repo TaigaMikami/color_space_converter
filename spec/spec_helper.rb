22c3c76eda20bf66d12ab4756c564ff4cc947e10
TaigaMikami <mikamimikami60@gmail.com>
2018-12-21 17:58:34 +0900

    add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require "bundler/setup"
 require "color_space_converter"
 
+require 'coveralls'
+Coveralls.wear!
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
