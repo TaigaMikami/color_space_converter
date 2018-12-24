f4792895228899e43f7dc42815fae118d305f1ef
TaigaMikami <mikamimikami60@gmail.com>
2018-12-25 00:55:22 +0900

    add rgb2xyz ref: http://w3.kcua.ac.jp/~fujiwara/infosci/colorspace/colorspace2.html

--- spec/color_space_converter/compute_spec.rb
+++ spec/color_space_converter/compute_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe ColorSpaceConverter::Compute do
     subject { cs.hex2rgb('#000000')}
     it { expect(subject).to eq([0, 0, 0])}
   end
+
+  context '#rgb2xyz' do
+    subject { cs.rgb2xyz(128, 128, 128)}
+    it { expect(subject).to eq([20.51754053582612, 21.586050011389922, 23.507208462403632]) }
+  end
+
+  context '#xyz2rgb' do
+    subject { cs.xyz2rgb(20.51754053582612, 21.586050011389922, 23.507208462403632)}
+    it { expect(subject).to eq([128, 128, 128]) }
+  end
 end
