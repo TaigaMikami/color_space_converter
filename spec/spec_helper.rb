bffc1324fc93e1d39afead9f6d84aa915a5ceaa5
TaigaMikami <mikamimikami60@gmail.com>
2018-12-26 18:32:03 +0900

    fix setting coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,11 @@
-require "bundler/setup"
-Dir.glob(File.join(File.dirname(__FILE__), "../lib/**/*.rb")).each { |f| require f }
-
 require 'coveralls'
 Coveralls.wear!
 
-
+require "bundler/setup"
+Dir.glob(File.join(File.dirname(__FILE__), "../lib/**/*.rb")).each { |f| require f }
 
 RSpec.configure do |config|
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
