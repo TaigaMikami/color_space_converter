ede13c5b645f6cff6077e9a9d81baf5920cc45c0
TaigaMikami <mikamimikami60@gmail.com>
2018-12-26 17:32:06 +0900

    add class test

--- spec/color_space_converter/color_space_spec.rb
+++ spec/color_space_converter/color_space_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
-Rspec.describe ColorSpaceConverter::ColorSpace do
+RSpec.describe ColorSpaceConverter::ColorSpace do
+  context "#new('rgb', 128, 128, 128)" do
+    let(:cs) { ColorSpaceConverter::ColorSpace.new('rgb', 128, 128, 128) }
+
+    context 'objecrt' do
+      it { expect(cs).to be_an_instance_of(ColorSpaceConverter::ColorSpace) }
+    end
+
+    context '@rgb' do
+      it { expect(cs.rgb).to eq ([128, 128, 128])}
+    end
+  end
 end
