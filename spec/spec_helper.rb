3c97165a53dab8ba620a158b1992ccd2b70180aa
TaigaMikami <mikamimikami60@gmail.com>
2018-12-27 02:10:30 +0900

    fix rubocop indication

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ require "bundler/setup"
 Dir.glob(File.join(File.dirname(__FILE__), "../lib/**/*.rb")).each { |f| require f }
 
 RSpec.configure do |config|
-
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
